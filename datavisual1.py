1156f37e253ba2b1707562df49a181992c054f92
archit-55 <architagrawal55@gmail.com>
2019-06-20 02:06:24 +0530

    Update datavisual1.py

--- datavisual1.py
+++ datavisual1.py
@@ -1,3 +1,17 @@
+
+Q.  data_visualization: 1
+
+visualize data graphs  
+i)  take input from  a file where you have 4 rows and 5 columns
+ii)  columns having - student_name , marks , age , contact , study_hours
+iii)  visualize this data as pie chart
+iv)  file name must  student.csv with all column separated by ','
+
+
+
+
+
+
 #!/usr/bin/python3
 
 
